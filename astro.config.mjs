92beefea4f1eb1e108c320bd9c7ff101e0f4b36a
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-09-16 18:35:18 +0545

    fix: add install

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,16 +6,16 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Hyde',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/HyDE-Project/Hyde-cli',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Install', slug: 'guides/install' },
 					],
 				},
 				{
